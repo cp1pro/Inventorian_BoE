b51578a43a07d288e43202565ed6cde6582940f8
cp1pro <notmeimba@gmail.com>
2022-12-12 02:50:07 +0300

    Fix error
    Code optimization. Error correction.

--- Inventorian_BoE/Inventorian_BoE.lua
+++ Inventorian_BoE/Inventorian_BoE.lua
@@ -1,53 +1,38 @@
 local ADDON = LibStub("AceAddon-3.0"):GetAddon("Inventorian")
-local InvBoE = ADDON:NewModule('InventorianBoE')
+local InvBoE = ADDON:NewModule('InventorianFeatures')
 
-function InvBoE:Update()
-	local item = self
+function InvBoE:Item_SetItem()
+	self.ItemBoe:Hide()
 	
-	if self:IsVisible() then
-		self.ItemBoe:Hide()
+	if not self.item or self:IsCached() then --Slot is Empty or Offline Mode (dont have required informatio)
+		return
 	end
-	
-	local _, _, _, quality, _, _, link, _, itemID, isBound = self:GetInfo()
-	--slot is empty or item is bound already, dont do nothing
 
-	if not itemID or isBound then return end
-	
-	local function Check(self, quality, itemID)
+	local _, _, _, quality, _, _, link, _, itemID, isBound = self:GetInfo()
+	if quality and isBound == false then
 		local _, _, _, _, _, itemClass = GetItemInfoInstant(itemID)
 		if quality >= Enum.ItemQuality.Uncommon and (itemClass == Enum.ItemClass.Weapon or itemClass == Enum.ItemClass.Armor) then
 			local r, g, b, hex = GetItemQualityColor(quality)
-			item.ItemBoe:SetFormattedText('|c%s%s|r', hex, 'BoE')
-			item.ItemBoe:Show()
+			self.ItemBoe:SetFormattedText('|c%s%s|r', hex, 'BoE')
+			self.ItemBoe:Show()
 		end
 	end
-	
-	if quality then
-		Check(self, quality, itemID)
-	else
-		local _item = Item:CreateFromBagAndSlot(self.bag, self.slot)
-		_item:ContinueOnItemLoad(function()
-			quality = select(4, self:GetInfo())
-			Check(self, (quality and quality or 0), itemID)
-		end)
-	end
 end
-
 function InvBoE:WrapItemButton(item)
 	if not item.ItemBoe then
-		local overlayFrame = CreateFrame("FRAME", nil, item)
-		overlayFrame:SetFrameLevel(4)
-		overlayFrame:SetAllPoints()
+		local BoeFrame = CreateFrame("FRAME", 'BOEFrame', item)
+		BoeFrame:SetFrameLevel(4)
+		BoeFrame:SetAllPoints()
 		
-		item.ItemBoe = overlayFrame:CreateFontString('$parentItemLevel', 'ARTWORK')
+		item.ItemBoe = BoeFrame:CreateFontString('$parentItemLevel', 'ARTWORK')
 		item.ItemBoe:SetPoint('BOTTOM', 0, 0)
 		item.ItemBoe:SetFontObject(NumberFontNormalSmall)
 		item.ItemBoe:SetJustifyH('CENTER')
 		item.ItemBoe:Show()
 	end
 	
-	hooksecurefunc(item, "Update", InvBoE.Update)
-
+	hooksecurefunc(item, "SetItem", InvBoE.Item_SetItem)
+	item:HookScript("OnEvent", InvBoE.Item_SetItem, LE_SCRIPT_BINDING_TYPE_INTRINSIC_POSTCALL)
 	return item
 end
 hooksecurefunc(ADDON.Item, "WrapItemButton", InvBoE.WrapItemButton)
